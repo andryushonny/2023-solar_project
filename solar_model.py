1cb0f7f1581d25190b59174a85eae71320f0f2fd
Arseniy Goryaev <goryaev@mail.ru>
2023-11-30 14:57:22 +0300

    fix: gui renewed

--- solar_model.py
+++ solar_model.py
@@ -7,44 +7,37 @@ gravitational_constant = 6.67408E-11
 
 def calculate_force(body, space_objects):
     """Вычисляет силу, действующую на тело.
-
     Параметры:
-
     **body** — тело, для которого нужно вычислить дейстующую силу.
     **space_objects** — список объектов, которые воздействуют на тело.
     """
-
+    G = gravitational_constant
     body.Fx = body.Fy = 0
     for obj in space_objects:
         if body == obj:
-            continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += -((body.x-obj.x)/r)*gravitational_constant*(body.m*obj.m)/(r**2)
-        body.Fy += -((body.y-obj.y)/r)*gravitational_constant*(body.m*obj.m)/(r**2)
+            continue
+        r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** 0.5
+        body.Fx -= G * body.m * obj.m / r ** 2 * (body.x - obj.x) / r
+        body.Fy -= G * body.m * obj.m / r ** 2 * (body.y - obj.y) / r
 
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
-
     Параметры:
-
     **body** — тело, которое нужно переместить.
     """
 
-    ax = body.Fx/body.m
+    ax = body.Fx / body.m
     ay = body.Fy / body.m
-    body.x += body.Vx * dt  # FIXME: не понимаю как менять...
-    body.y += body.Vy * dt
+    body.x += body.Vx * dt + ax * dt ** 2 / 2
     body.Vx += ax * dt
-    body.Vx += ay * dt
-    # FIXME: not done recalculation of y coordinate!
+    body.y += body.Vy * dt + ay * dt ** 2 / 2
+    body.Vy += ay * dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
-
     Параметры:
-
     **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
     **dt** — шаг по времени
     """
